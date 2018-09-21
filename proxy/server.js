4e81a357baa99c10624f21ba1e4e2ad87fd0d72e
canderson22 <charles.anderson.dev@gmail.com>
2018-09-21 11:11:32 -0700

    update

--- proxy/server.js
+++ proxy/server.js
@@ -3,21 +3,28 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+var proxy = require('express-http-proxy');
+
 const PORT = process.env.PORT || 3000;
 
 // Allow cross origin requests
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/:company', express.static(path.join(__dirname, 'public')));
+app.use('/api', proxy('localhost:3001'));
+app.use('/data/company', proxy('localhost:3002'));
+app.use('/people-also-bought', proxy('localhost:3003'));
+app.use('/stocks/sideBar', proxy('localhost:3004'));
 
 app.listen(PORT, err =>
   console.log(
